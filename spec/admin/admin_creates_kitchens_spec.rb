642f2f9a460618300a63991dd628f8677be004ec
Guilherme Labrego <glabrego@gmail.com>
2016-02-08 17:31:01 -0200

    Implemented admin creates new kitchens tests.

---
+++ spec/admin/admin_creates_kitchens_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'Admin creates new kitchens' do
+  scenario 'successfully' do
+
+    user = FactoryGirl.create(:user)
+    kitchen = FactoryGirl.build(:kitchen)
+
+    visit new_user_session_path
+
+    fill_in 'Email',     with: user.email
+    fill_in 'Password', with: user.password
+
+    click_on 'Log in'
+
+
+    visit new_kitchen_path
+
+    fill_in 'Name', with: kitchen.name
+
+    click_on 'Criar Cozinha'
+
+    expect(page).to have_content kitchen.name
+  end
+end
---
+++ spec/support/model_macros.rb
@@ -0,0 +1,24 @@
+module ModelMacros
+
+  def login_admin
+      user = FactoryGirl.create(:user)
+
+      visit new_user_session_path
+
+      fill_in 'Email',     with: user.email
+      fill_in 'Password', with: user.password
+
+      click_on 'Log in'
+  end
+
+  def login_user
+    user = FactoryGirl.create(:user, admin: false)
+
+    visit new_user_session_path
+
+    fill_in 'Email',     with: user.email
+    fill_in 'Password', with: user.password
+
+    click_on 'Log in'
+  end
+end

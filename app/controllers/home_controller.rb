d8b74a829f46cce16122f1bbf1799605a91897bc
Guilherme Labrego <glabrego@gmail.com>
2016-02-08 23:11:44 -0200

    Users now can see latest recipes at home.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @kitchens = Kitchen.all
-    @food_types = FoodType.all
+    @food_types = FoodType.last(20)
     @preferences = Preference.all
     @recipes = Recipe.all
   end
---
+++ spec/user/user_see_latest_recipes_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'User creates recipes' do
+  scenario 'successfully' do
+    recipes = %w[Arroz Feijão Batata Macarrão Strogonoff Sushi Salmão Empanada Torta
+    Lasana Pizza Esfiha Coxinha Rabanada Pudim Nuggets Sopa Café Pão Bolo]
+
+    recipes.each do |name|
+      FactoryGirl.create(:recipe, name: name)
+    end
+
+    visit root_path
+
+    recipes.each do |name|
+      expect(page).to have_content name
+    end
+  end
+end

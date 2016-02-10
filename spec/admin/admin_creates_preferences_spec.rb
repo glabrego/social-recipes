a51b20a1fb7025d202dd6acb12e5fb0404524ff8
Guilherme Labrego <glabrego@gmail.com>
2016-02-10 18:17:13 -0200

    Refactored admin tests.

--- spec/admin/admin_creates_preferences_spec.rb
+++ spec/admin/admin_creates_preferences_spec.rb
@@ -2,15 +2,9 @@ require 'rails_helper'
 
 feature 'Admin creates preferences' do
   scenario 'successfully' do
-    user = FactoryGirl.create(:user)
     preference = FactoryGirl.build(:preference)
 
-    visit new_user_session_path
-
-    fill_in 'Email',     with: user.email
-    fill_in 'Password', with: user.password
-
-    click_on 'Log in'
+    login_admin
 
     visit new_preference_path
 
@@ -22,15 +16,9 @@ feature 'Admin creates preferences' do
   end
 
   scenario 'and not admin users cannot creates new food types' do
-    user = FactoryGirl.create(:user, admin: false)
     preference = FactoryGirl.build(:preference)
 
-    visit new_user_session_path
-
-    fill_in 'Email',     with: user.email
-    fill_in 'Password', with: user.password
-
-    click_on 'Log in'
+    login_user
 
     visit new_preference_path
 

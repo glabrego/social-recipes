d8c3d6f6a3e1b7c2d2d5b3c5c7fdf5a6bd549a34
Guilherme Labrego <glabrego@gmail.com>
2016-02-11 14:07:18 -0200

    Users now can see favorited kitchens at user page.

--- spec/user/user_favorite_kitchens_spec.rb
+++ spec/user/user_favorite_kitchens_spec.rb
@@ -11,4 +11,30 @@ feature 'User favorite kitchen' do
 
     expect(page).to have_content 'Cozinha adicionada as favoritas!'
   end
+
+  scenario 'and remove favorited kitchen' do
+    kitchen = FactoryGirl.create(:kitchen)
+
+    login_user
+
+    visit kitchen_path(kitchen)
+    click_on 'Favoritar'
+    click_on 'Remover Favorita'
+
+    expect(page).to have_content 'Cozinha removida das favoritas!'
+  end
+
+  scenario 'and see users favorites' do
+    kitchen = FactoryGirl.create(:kitchen)
+    user = FactoryGirl.create(:user)
+
+    login_user(user)
+
+    visit kitchen_path(kitchen)
+    click_on 'Favoritar'
+
+    visit user_path(user)
+
+    expect(page).to have_content(kitchen.name)
+  end
 end
